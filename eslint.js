b511c31b845fa51f6b942c38e26c3a7db3ceb877
muhammadyunusuz <yunuzmuhammad1@gmail.com>
2022-03-23 05:13:10 +0500

    add comments

--- eslint.js
+++ eslint.js
@@ -3,19 +3,18 @@ const path = require("path");
 const paths = ["packages/mypackage/src/text/en.json"];
 
 function getFiles(dir, files_) {
+	// Bu funksiya barcha papka ichida ichma ich joylashgan ts, tsx fayllarni yig'ib beradi
 	files_ = files_ || [];
 	var files = fs.readdirSync(dir);
+	// Papkani o'qish uchun
 	for (var i in files) {
 		var name = dir + "/" + files[i];
+		// Papkalarni ichida papka chiqib qolsa recursive funksiya ishlatamiz
 		if (fs.statSync(name).isDirectory()) {
 			getFiles(name, files_);
 		} else {
-			if (
-				name.endsWith(".js") ||
-				name.endsWith(".ts") ||
-				name.endsWith(".tsx") ||
-				name.endsWith(".jsx")
-			) {
+			// fayllarni topsak qo'shamiz
+			if (name.endsWith(".ts") || name.endsWith(".tsx")) {
 				files_.push(name);
 			}
 		}
@@ -24,21 +23,29 @@ function getFiles(dir, files_) {
 }
 
 function checkIsUsedOrNot(array) {
+	// Bu funksiya tilning faylidagi properties ni iteratsiya qilib, tepadagi funksiya topgan fayllar ichidan qidirib chiqadi.
 	const notUsedKeys = [];
 	for (let lgFilePath of array) {
 		const parentPath = path.join(lgFilePath, "..", "..", "..");
+		// Har bir til faylni package papkasini topib olishimiz kerak
 		if (!parentPath.startsWith("packages")) return;
+		// Agar shu papka packagesni ichida bo'lmasa to'xtatadi;
 		let allFiles = getFiles(parentPath);
+		// Hamma ichma ich ts, tsx fayllarni olib oldik
 		let jsonKeys = JSON.parse(
 			fs.readFileSync(path.join(__dirname, lgFilePath), "utf-8")
 		);
+		// Til faylini js object shakliga o'tkazdim
 		for (let item in jsonKeys) {
+			// Har bir til propertisini tekshirib chiqamiz
 			let used = false;
 			for (const i of allFiles) {
 				let fileBody = fs.readFileSync(
 					path.join(__dirname, i),
 					"utf-8"
 				);
+				// faylni o'qiymiz
+				// Agar ichida shu so'z ishlatilgan bo'lsa qo'shmaymiz, bo'lmasa ro'yxatga qo'shamiz
 				if (fileBody.includes(item)) {
 					used = true;
 					break;
@@ -53,7 +60,7 @@ function checkIsUsedOrNot(array) {
 				});
 		}
 	}
-
+	// Qaytarib beramiz
 	return notUsedKeys;
 }
 
